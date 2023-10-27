2f2a3d3e060a6012225dabf93151aa16eba8d29a
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-10-28 02:16:49 +0530

    Update Winning Scenario
    Failed Case for previous function
    -> Whenever the game goes beyond 3 chances for each player, the winning scenario was not firing up.

--- script.js
+++ script.js
@@ -58,18 +58,19 @@ function startGame(val) {
 
 function checkResult(ele) {
     for (let i in winningScenarios) {
-        if (ignoreOrderCompare(winningScenarios[i], playersInfo[ele].entries)) console.log("Winning Situation")
+        if (winningScenarios[i].every(val => playersInfo[ele].entries.includes(val)) == true) {
+            console.log("Winning Scenario")
+            console.log(playersInfo[ele].displayName, "wins")
+        }
+        // if (ignoreOrderCompare(winningScenarios[i], playersInfo[ele].entries)) {
+        //     console.log("Winning Situation")
+        //     
+        // }
     }
 }
 
-
-const ignoreOrderCompare = (a, b) => {
-    if (a.length !== b.length) return false;
-    const elements = new Set([...a, ...b]);
-    for (const x of elements) {
-        const count1 = a.filter(e => e === x).length;
-        const count2 = b.filter(e => e === x).length;
-        if (count1 !== count2) return false;
-    }
-    return true;
+let checkSubset = (parentArray, subsetArray) => {
+    return subsetArray.every((el) => {
+        return parentArray.includes(el)
+    })
 }
