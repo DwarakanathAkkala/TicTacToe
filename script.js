df86b8c4d2b1273b6b5a285927485f375b581ce0
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-10-31 00:15:49 +0530

    Changes
    1. Update Preferences if and only if "Game On" button is clicked.
    2. Added Winning Toast using DOM.
    3. Updated Player2 Name event listener.
    4. Added Change Preferences button.
    5. Storing Current Game Preferences

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
+let container = document.createElement("div");
+container.setAttribute("class", "container");
+container.innerHTML = `
+    <div class="toast-container position-fixed top-50 start-50 translate-middle" style="z-index: 0">
+        <div id="winnerToast" class="toast hide" role="alert" aria-live="assertive" aria-atomic="true">
+            <div class="toast-header">
+                <strong class="mt-auto">Hooray!!</strong>
+            </div>
+            <div class="toast-body bg-success text-white" id="winnerMsg">
+                
+            </div>
+        </div>
+    </div>
+`;
+
+// Add Created DOM to HTML
+document.body.append(container);
+
+let winMsgElement = document.getElementById("winnerToast");
+let winMsgToast = new bootstrap.Toast(winMsgElement, {
+    delay: 800
+});
+
 let player1Input = document.getElementsByName("player1")[0];
 let player2Input = document.getElementsByName("player2")[0];
 
 // Event Listeners for Player Name Preferences
 player1Input.addEventListener('change', () => { player1Input.value !== (null || undefined) ? playersInfo["player1"].displayName = player1Input.value : "Player 1"; });
 
-player2Input.addEventListener('change', () => { player1Input.value !== (null || undefined) ? playersInfo["player2"].displayName = player1Input.value : "Player 2"; });
+player2Input.addEventListener('change', () => { player2Input.value !== (null || undefined) ? playersInfo["player2"].displayName = player2Input.value : "Player 2"; });
 
 
 function choose() {
+
+    // PlayerInfo Updation
+    for (i in playersInfo) {
+        i == "player1" ? playersInfo[i].sign = player1Sign.innerText : playersInfo[i].sign;
+        i == "player2" ? playersInfo[i].sign = player2Sign.innerText : playersInfo[i].sign;
+    }
+
     document.getElementById("playGameBtn").style.display = "none";
     document.getElementById("restartBtn").style.display = "block";
+    document.getElementById("changePref").style.display = "block";
 
     for (let i in inputElements) {
         inputElements[i].value == ("" || undefined) ? (inputElements[i].value = "", inputElements[i].disabled = false) : (inputElements[i].value = " ", inputElements[i].disabled = false);
     }
 
-    player1Input.value = "Player 1";
-    player2Input.value = "Player 2";
-    console.log(player1Input.value, " vs ", player2Input.value)
+    console.log(player1Input.value, " vs ", player2Input.value);
+
+
     gameOn();
+
+    // Store current game preferences
+    currGameChoice = playersInfo;
+    console.log("Choices at Start", currGameChoice);
 }
 
 window.onload = () => {
@@ -43,6 +78,7 @@ let inputElements = document.getElementsByClassName("gameGrid");
 
 let flag = false;
 let currTurn = "player1";
+let currGameChoice;
 let playersInfo = {
     "player1": {
         "displayName": "Player 1",
@@ -76,15 +112,24 @@ function startGame(val) {
     }
 }
 
+
+
 function checkResult(ele) {
     for (let i in winningScenarios) {
         if (winningScenarios[i].every(val => playersInfo[ele].entries.includes(val)) == true) {
             console.log(playersInfo[ele].entries)
-            console.log("Winning Scenario")
-            console.log(playersInfo[ele].displayName, "wins")
+            console.log("Winning Scenario");
+            console.log(playersInfo[ele])
+
+            document.getElementById("winnerMsg").innerText = playersInfo[ele].displayName + " Wins";
+            winMsgToast.show();
+
+
             for (let i in inputElements) inputElements[i].disabled = true;
             document.getElementById("playGameBtn").style.display = "none";
             document.getElementById("restartBtn").style.display = "block";
+            document.getElementById("changePref").style.display = "block";
+            return;
         }
         else if (playersInfo[ele].entries.length > 4 && winningScenarios[i].every(val => playersInfo[ele].entries.includes(val)) == false) {
             console.log("Draw")
@@ -105,12 +150,6 @@ function changePreference() {
     // DOM Updation
     player1Sign.innerText == "X" ? player1Sign.innerText = "O" : player1Sign.innerText = "X";
     player2Sign.innerText == "O" ? player2Sign.innerText = "X" : player2Sign.innerText = "O";
-
-    // PlayerInfo Updation
-    for (i in playersInfo) {
-        i == "player1" ? playersInfo[i].sign = player1Sign.innerText : playersInfo[i].sign;
-        i == "player2" ? playersInfo[i].sign = player2Sign.innerText : playersInfo[i].sign;
-    }
 }
 
 
@@ -126,6 +165,10 @@ function gameOn() {
 }
 
 function restartGame() {
+
+    playersInfo = currGameChoice;
+    console.log("Choices after Restart", currGameChoice);
+
     // Reset Player Entries
     playersInfo["player1"].entries = [];
     playersInfo["player2"].entries = [];
@@ -141,3 +184,4 @@ function returnMainMenu() {
     document.getElementById("playGameBtn").style.display = "block";
     document.getElementById("restartBtn").style.display = "none";
 }
+
