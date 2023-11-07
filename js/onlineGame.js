ac232df13af746dd64ae9ccb7aac5747306a428f
DwarakanathAkkala <dwarakanath.akkala@gmail.com>
2023-11-07 18:40:15 +0530

    Socket Connection for 2 Players
    1. Search for players on clicking "Search for Player" button.
    2. Match Players

--- js/onlineGame.js
+++ js/onlineGame.js
@@ -5,24 +5,40 @@ document.getElementById("onlinePlayGameBtn").addEventListener("click", () => {
     document.getElementById("localMode").style.display = "none";
 })
 
-// let name = document.getElementById("yourName").value;
+let userName;
 
-// socket.emit("find", { name: name });
 
-// socket.on('find', (e) => {
-//     console.log("Socket IO Func")
-//     let allPlayersArray = e.allPlayers
-//     console.log(allPlayersArray)
+document.getElementById('findPlayer').addEventListener("click", function () {
+    userName = document.getElementById("yourName").value;
+    document.getElementById("user").innerText = userName
 
-//     let oppName;
-//     let value;
+    socket.emit("find", { name: userName })
+    document.getElementById("findPlayer").disabled = true;
 
-//     const foundObj = allPlayersArray.find(obj => obj.player1.displayName == `${name}` || obj.player2.displayName == `${name}`)
+    socket.on('find', (e) => {
+        console.log("Socket IO Func")
+        let allPlayersArray = e.allPlayers
+        console.log(allPlayersArray)
 
-//     foundObj.player1.displayName == `${name}` ? oppName = foundObj.player2.displayName : oppName = foundObj.player1.displayName;
-//     foundObj.player1.displayName == `${name}` ? value = foundObj.player2.sign : value = foundObj.player1.sign;
+        let oppName;
+        let value;
 
-//     console.log("Opp Name", oppName);
-//     console.log("Value", value);
+        const foundObj = allPlayersArray.find(obj => obj.player1.displayName == userName || obj.player2.displayName == userName)
 
-// })
+        foundObj.player1.displayName == userName ? oppName = foundObj.player2.displayName : oppName = foundObj.player1.displayName;
+        foundObj.player1.displayName == userName ? value = foundObj.player2.sign : value = foundObj.player1.sign;
+
+        document.getElementById("user").value = userName;
+
+        console.log("Opp Name", oppName);
+        console.log("Value", value);
+
+    })
+
+})
+
+
+
+
+
+
